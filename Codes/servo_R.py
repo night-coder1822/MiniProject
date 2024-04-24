5c27bb6facb3d715db2d2c25526e96ebb1ae880a
Night Coder <himanshunagose1806@gmail.com>
2024-04-24 09:39:43 +0530

    Servo control code updated

--- Codes/servo_R.py
+++ Codes/servo_R.py
@@ -1,12 +1,21 @@
-from gpiozero import AngularServo
+from gpiozero import Servo
 from time import sleep
 
-servo=AngularServo(18, min_pulse_width=0.00006, max_pulse_width=0.0023)
+# Connect the signal wire of the servo to GPIO pin 18 (BCM numbering)
+servo = Servo(18)
 
 while True:
-    servo.angle=90
+    # Move the servo to the minimum angle (usually -90 degrees)
+    servo.min()
+    print("Set to minimum angle")
     sleep(1)
-    servo.angle=0
+    
+    # Move the servo to the center position (usually 0 degrees)
+    servo.mid()
+    print("Set to center position")
     sleep(1)
-    servo.angle=-90
+    
+    # Move the servo to the maximum angle (usually +90 degrees)
+    servo.max()
+    print("Set to maximum angle")
     sleep(1)
